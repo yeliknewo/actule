5071e88fbd94f391a2fb7d01304e4a35bc05a652
Kiley Owen <yelikdev@gmail.com>
2016-05-27 19:22:27 -0400

    Version 0.4.2

--- src/actule/components/transform.rs
+++ src/actule/components/transform.rs
@@ -11,12 +11,26 @@ pub struct Transform {
 }
 
 impl Transform {
-    pub fn new(position: Vec2d, rotation: Coord, scale: Vec2d) -> Transform {
+    #[inline]
+    pub fn new_isometry(isometry: Isometry2<Coord>, scale: Vector2<Coord>) -> Transform {
         Transform {
-            isometry2: Isometry2::new(Vector2::new(position[0], position[1]), Vector1::new(rotation)),
-            scale: Vector2::new(scale[0], scale[1]),
+            isometry2: isometry,
+            scale: scale,
             dirty: true,
         }
+    }
+
+    #[inline]
+    pub fn new(position: Vector2<Coord>, rotation: Vector1<Coord>, scale: Vector2<Coord>) -> Transform {
+        Transform::new_isometry(
+            Isometry2::new(position, rotation),
+            scale
+        )
+    }
+
+    #[inline]
+    pub fn new_piston(position: Vec2d, rotation: Coord, scale: Vec2d) -> Transform {
+        Transform::new(Vector2::new(position[0], position[1]), Vector1::new(rotation), Vector2::new(scale[0], scale[1]))
     }
 
     pub fn tick(&mut self, renderable: &mut Box<Renderable>) {
