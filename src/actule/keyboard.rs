082a0a9b1115413c10d7c9fe6f0edd02bfefd988
Kiley Owen <yelikdev@gmail.com>
2016-05-30 17:54:31 -0400

    Added get key_state

--- src/actule/keyboard.rs
+++ src/actule/keyboard.rs
@@ -20,11 +20,21 @@ impl Keyboard {
             state: HashMap::new()
         }
     }
+
     pub fn set_key(&mut self, key: Key, key_state: KeyState) {
         self.state.insert(key, key_state);
 
     }
+
     pub fn read_keystate(&self, key: Key) -> Option<&KeyState> {
         self.state.get(&key)
+    }
+
+    pub fn get_key_state(&self, key: Key) -> KeyState {
+        if let Some(state) = self.state.get(&key) {
+            *state
+        } else {
+            KeyState::Released
+        }
     }
 }
