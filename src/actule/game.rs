f98ec38844a4cbfedc464f81b0940742ba404fd3
bluecereal <fuck off dis my email>
2016-05-30 15:59:26 -0400

    version 0.4.4

--- src/actule/game.rs
+++ src/actule/game.rs
@@ -1,6 +1,7 @@
 use std::hash::Hash;
 
-use piston_window::{Event, PistonWindow, clear, UpdateEvent, BuildFromWindowSettings, Window, AdvancedWindow, OpenGLWindow, GenericEvent};
+//use piston_window::{Event, PistonWindow, clear, UpdateEvent, BuildFromWindowSettings, Window, AdvancedWindow, OpenGLWindow, GenericEvent};
+use piston_window::*;
 use id_alloc::*;
 
 use actule::*;
@@ -31,28 +32,29 @@ impl<I: Num + Bounded + Ord + CheckedAdd + CheckedSub + One + Copy + Hash, T: En
 
     pub fn run<W>(&mut self, manager: &mut Node<I>, window: &mut PistonWindow<W>) where W: BuildFromWindowSettings + Window + AdvancedWindow + OpenGLWindow, W::Event: GenericEvent {
         while let Some(e) = window.next() {
-            match e {
-                Event::Render(args) => {
-                    window.draw_2d(&e, |c, g| {
-                        clear([0.0, 0.0, 0.0, 1.0], g);
-                        for layer in self.world.get_active_layers() {
-                            for entity_id in self.world.get_render_ids().get(layer).expect("Active layer wasn't a layer").iter() {
-                                if let Some(entity) = self.world.get_entity_by_id(*entity_id) {
-                                    if let Some(renderable) = entity.get_renderable() {
-                                        renderable.draw_2d(c, g);
-                                    }
-                                }
+            window.draw_2d(&e, |c, g| {
+                clear([0.0, 0.0, 0.0, 1.0], g);
+                for layer in self.world.get_active_layers() {
+                    for entity_id in self.world.get_render_ids().get(layer).expect("Active layer wasn't a layer").iter() {
+                        if let Some(entity) = self.world.get_entity_by_id(*entity_id) {
+                            if let Some(renderable) = entity.get_renderable() {
+                                renderable.draw_2d(c, g);
                             }
                         }
-                    });
+                    }
                 }
-                Event::Input(input) => self.keyboard.update(input),
+            });
+            if let Some(Button::Keyboard(key)) = e.press_args() {
+                self.keyboard.set_key(key, KeyState::Pressed);
+            }
+            if let Some(Button::Keyboard(key)) = e.release_args() {
+                self.keyboard.set_key(key, KeyState::Released);
             }
             if let Some(args) = e.update_args() {
                 if let Some(tick_ids) = self.world.take_tick_ids() {
                     for id in tick_ids.iter() {
                         if let Some(mut entity) = self.world.take_entity_by_id(*id) {
-                            entity.tick(args.dt, manager, &mut self.world);
+                            entity.tick(args.dt, manager, &mut self.world, &mut self.keyboard);
                             self.world.give_entity(entity);
                         }
                     }
--- src/actule/keyboard.rs
+++ src/actule/keyboard.rs
@@ -2,10 +2,6 @@
 use piston_window::*;
 use std::collections::HashMap;
 
-
-
-const ESC: Button = Button::Keyboard(Key::Escape);
-
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum KeyState {
     Pressed,
@@ -14,38 +10,21 @@ pub enum KeyState {
 
 #[derive(Debug)]
 pub struct Keyboard {
-    state: HashMap<Button, KeyState>
+    state: HashMap<Key, KeyState>
 }
 
 impl Keyboard {
     pub fn new() -> Keyboard {
         //should Keyboard start with some keys like ESC and arrow keys already inserted?
-        let mut start_state = HashMap::new();
-        start_state.insert(ESC, KeyState::Released);
         Keyboard {
-            state: start_state
+            state: HashMap::new()
         }
     }
-    pub fn new_key(&mut self, new_key: Button) {
-        self.state.insert(new_key, KeyState::Released);
-        println!("added a new key {:?}", new_key);
-    }
-    pub fn set_key(&mut self, key: Button, key_state: KeyState) {
+    pub fn set_key(&mut self, key: Key, key_state: KeyState) {
         self.state.insert(key, key_state);
 
     }
-    pub fn read_keystate(&self, key: Button) -> &KeyState {
-        self.state.get(&key).expect("key does not exist")
-    }
-    pub fn update(&mut self, input: Input) {
-        match input {
-            Input::Press(button) => {
-                self.set_key(button, KeyState::Pressed)
-            }
-            Input::Release(button) => {
-                self.set_key(button, KeyState::Released)
-            }
-            _ => ()
-        }
+    pub fn read_keystate(&self, key: Key) -> Option<&KeyState> {
+        self.state.get(&key)
     }
 }
