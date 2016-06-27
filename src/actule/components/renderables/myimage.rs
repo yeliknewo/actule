fe3e0971cad2712ef44b501c264b5bb2977e0e75
bluecereal <fuck off dis my email>
2016-06-27 19:50:21 -0400

    added matrix to myimage

--- src/actule/components/renderables/myimage.rs
+++ src/actule/components/renderables/myimage.rs
@@ -5,12 +5,15 @@ use piston_window::image::*;
 use piston_window::{Texture, Flip, TextureSettings};
 use std::path::Path;
 
+use piston_window::math::{Matrix2d, Vec2d, identity, multiply};
+
 //use piston_window::DrawState;
 
 
 pub struct MyImage {
+    matrix: Matrix2d,
     texture: Texture<Resources>,
-    dimensions: [f64; 4], //[x, y, w, h]
+    //dimensions: [f64; 4], //[x, y, w, h]
     image: Image
 }
 
@@ -20,13 +23,14 @@ impl MyImage {
         let texture = Texture::from_path(factory, Path::new(path), Flip::None, &TextureSettings::new()).unwrap();
 
         MyImage {
+            matrix: identity(),
             texture: texture,
-            dimensions: dimensions,
+            //dimensions: dimensions,
             image: image
         }
     }
 
     pub fn draw_2d(&self, c: Context, g: &mut G2d) {
-        self.image.draw(&self.texture, &c.draw_state, c.transform, g);
+        self.image.draw(&self.texture, &c.draw_state, multiply(c.transform, self.matrix), g);
     }
 }
