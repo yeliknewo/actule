1a1e55dd18efc35afaa5332e3e7302b4300e91b3
bluecereal <fuck off dis my email>
2016-06-26 22:42:39 -0400

    added Image type to MyImage

--- src/actule/components/renderables/myimage.rs
+++ src/actule/components/renderables/myimage.rs
@@ -1,13 +1,13 @@
 use piston_window::{Context, G2d, Texture, TextureSettings};
 pub use gfx_device_gl::{Resources, Factory};
-use piston_window::image;
-use piston_window::draw_state::Stencil;
+use piston_window::image::*;
 //use piston_window::DrawState;
 
 
 pub struct MyImage {
     texture: Texture<Resources>,
-    dimensions: [u32; 4] //[x, y, w, h]
+    dimensions: [u32; 4], //[x, y, w, h]
+    image: Image
 }
 
 impl MyImage {
@@ -16,17 +16,14 @@ impl MyImage {
     //images can be created from paths, etc etc.  that can be added as needed?
     pub fn new(factory: &mut Factory, buffer: &[u8], width: u32, height: u32, dimensions: [u32; 4]) -> MyImage {
         let texture = Texture::from_memory_alpha(factory, buffer, width, height, &TextureSettings::new()).unwrap();
+        let image = Image::new().rect([dimensions[0] as f64, dimensions[1] as f64, dimensions[2] as f64, dimensions[3] as f64]);
         MyImage {
             texture: texture,
-            dimensions: dimensions
+            dimensions: dimensions,
+            image: image
         }
     }
     pub fn draw_2d(&self, c: Context, g: &mut G2d) {
-        //this is not efficient at all!
-        //i think this breaks stuff
-        let mut temp_c = c;
-        temp_c.draw_state.stencil = Some(Stencil::Inside(255));
-        temp_c.draw_state.scissor(self.dimensions);
-        image(&self.texture, temp_c.transform, g);
+        self.image.draw(&self.texture, &c.draw_state, c.transform, g);
     }
 }
