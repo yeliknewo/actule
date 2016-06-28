3f27217ff6ba4387d8d56f3f39e91fb317e8ef4e
bluecereal <fuck off dis my email>
2016-06-28 14:04:09 -0400

    still trying to get images to move

--- src/actule/components/renderables/myimage.rs
+++ src/actule/components/renderables/myimage.rs
@@ -5,8 +5,11 @@ use piston_window::image::*;
 use piston_window::{Texture, Flip, TextureSettings};
 use std::path::Path;
 
-use piston_window::math::{Matrix2d, Vec2d, identity, multiply};
+use piston_window::math::{Matrix2d, identity, multiply};
+use piston_window::image;
 
+use piston_window::ImageSize;
+use piston_window::Transformed;
 //use piston_window::DrawState;
 
 
@@ -14,23 +17,24 @@ pub struct MyImage {
     matrix: Matrix2d,
     texture: Texture<Resources>,
     //dimensions: [f64; 4], //[x, y, w, h]
-    image: Image
+    //image: Image
 }
 
 impl MyImage {
     pub fn new(factory: &mut Factory, path: &str, dimensions: [f64; 4]) -> MyImage {
-        let image = Image::new().rect(dimensions);
+        //let image = Image::new().rect(dimensions);
         let texture = Texture::from_path(factory, Path::new(path), Flip::None, &TextureSettings::new()).unwrap();
 
         MyImage {
             matrix: identity(),
             texture: texture,
             //dimensions: dimensions,
-            image: image
+            //image: image
         }
     }
 
     pub fn draw_2d(&self, c: Context, g: &mut G2d) {
-        self.image.draw(&self.texture, &c.draw_state, multiply(c.transform, self.matrix), g);
+        let (spritex, spritey) = self.texture.get_size();
+        image(&self.texture, multiply(c.transform.trans(spritex as f64, spritey as f64), self.matrix), g);
     }
 }
