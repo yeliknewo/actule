dc9932413346e46d2db742097db064958e8f0447
bluecereal <fuck off dis my email>
2016-06-29 22:43:23 -0400

    everything works fine now!

--- src/actule/components/renderables/myimage.rs
+++ src/actule/components/renderables/myimage.rs
@@ -7,22 +7,28 @@ use std::path::Path;
 
 use piston_window::math::{Matrix2d, multiply};
 use piston_window::image;
+//use piston_window::DrawState;
 
 
 pub struct MyImage {
     texture: Texture<Resources>,
+    //dimensions: [f64; 4], //[x, y, w, h]
+    image: Image
 }
 
 impl MyImage {
-    pub fn new(factory: &mut Factory, path: &Path) -> MyImage {
+    pub fn new(factory: &mut Factory, path: &Path, dimensions: [f64; 4]) -> MyImage {
+        let image = Image::new().rect(dimensions);
         let texture = Texture::from_path(factory, path, Flip::None, &TextureSettings::new()).unwrap();
 
         MyImage {
             texture: texture,
+            //dimensions: dimensions,
+            image: image
         }
     }
 
     pub fn draw_2d(&self, c: Context, g: &mut G2d, matrix: Matrix2d) {
-        image(&self.texture, multiply(c.transform, matrix), g);
+        self.image.draw(&self.texture, &c.draw_state, multiply(c.transform, matrix), g);
     }
 }
